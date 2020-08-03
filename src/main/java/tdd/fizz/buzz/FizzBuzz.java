765543765255cd60417b11f60d4424183a2097ee
simon <hanzhenfeng@wuxicloud.com>
2020-08-03 08:53:02 +0800

    b practice

---
+++ src/main/java/tdd/fizz/buzz/FizzBuzz.java
@@ -0,0 +1,31 @@
+package tdd.fizz.buzz;
+
+/**
+ * @author Simon
+ * @date 2020/8/3 08:47
+ */
+public class FizzBuzz {
+    private final int number;
+
+    public FizzBuzz(int number) {
+        this.number = number;
+    }
+
+    public static String of(int number) {
+        FizzBuzz fizzBuzz = new FizzBuzz(number);
+        if (fizzBuzz.isDivisibleBy(3) && fizzBuzz.isDivisibleBy(5)) {
+            return "FizzBuzz";
+        }
+        if (fizzBuzz.isDivisibleBy(3)) {
+            return "Fizz";
+        }
+        if (fizzBuzz.isDivisibleBy(5)) {
+            return "Buzz";
+        }
+        return String.valueOf(number);
+    }
+
+    private boolean isDivisibleBy(int i) {
+        return this.number % i == 0;
+    }
+}
