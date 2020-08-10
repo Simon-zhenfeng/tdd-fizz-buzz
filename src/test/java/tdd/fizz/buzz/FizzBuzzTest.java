457593a15b799e4101427808606f7688ce687189
simon <hanzhenfeng@wuxicloud.com>
2020-08-10 09:55:06 +0800

    practice

---
+++ src/test/java/tdd/fizz/buzz/FizzBuzzTest.java
@@ -0,0 +1,31 @@
+package tdd.fizz.buzz;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Simon
+ * @date 2020/8/10 09:48
+ */
+public class FizzBuzzTest {
+    @Test
+    public void should_return_string_given_number() {
+        assertThat(FizzBuzz.of(1)).isEqualTo("1");
+    }
+
+    @Test
+    public void should_return_Fizz_given_number_is_divisible_by_3() {
+        assertThat(FizzBuzz.of(3)).isEqualTo("Fizz");
+    }
+
+    @Test
+    public void should_return_Buzz_given_number_is_divisible_by_5() {
+        assertThat(FizzBuzz.of(5)).isEqualTo("Buzz");
+    }
+
+    @Test
+    public void should_return_FizzBuzz_given_number_is_divisible_by_3_and_5() {
+        assertThat(FizzBuzz.of(15)).isEqualTo("FizzBuzz");
+    }
+}
