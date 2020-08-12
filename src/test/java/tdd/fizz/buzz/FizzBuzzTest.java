29bbdce647f586769cac38a9b74a93307a2bef09
simon <hanzhenfeng@wuxicloud.com>
2020-08-12 08:45:30 +0800

    practice

---
+++ src/test/java/tdd/fizz/buzz/FizzBuzzTest.java
@@ -0,0 +1,38 @@
+package tdd.fizz.buzz;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Simon
+ * @date 2020/8/12 08:38
+ */
+public class FizzBuzzTest {
+    @Test
+    public void should_return_string_given_number() {
+        assertThat(FizzBuzz.of(1)).isEqualTo("1");
+        assertThat(FizzBuzz.of(2)).isEqualTo("2");
+    }
+
+    @Test
+    public void should_return_Fizz_given_number_is_divisible_by_3() {
+        assertThat(FizzBuzz.of(3)).isEqualTo("Fizz");
+        assertThat(FizzBuzz.of(6)).isEqualTo("Fizz");
+        assertThat(FizzBuzz.of(9)).isEqualTo("Fizz");
+    }
+
+    @Test
+    public void should_return_Buzz_given_number_is_divisible_by_5() {
+        assertThat(FizzBuzz.of(5)).isEqualTo("Buzz");
+        assertThat(FizzBuzz.of(10)).isEqualTo("Buzz");
+        assertThat(FizzBuzz.of(20)).isEqualTo("Buzz");
+    }
+
+    @Test
+    public void should_return_FizzBuzz_given_number_is_divisible_by_3_and_5() {
+        assertThat(FizzBuzz.of(15)).isEqualTo("FizzBuzz");
+        assertThat(FizzBuzz.of(30)).isEqualTo("FizzBuzz");
+        assertThat(FizzBuzz.of(45)).isEqualTo("FizzBuzz");
+    }
+}
